edc3ff45ef536838eeb98e0d785f6c2a735f6cc8
duytan124 <duytan2005hg@gmail.com>
2026-03-09 23:05:07 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');
 const axios = require('axios');
 require('dotenv').config();
 
